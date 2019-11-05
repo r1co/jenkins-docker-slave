8412f7f76fd7cea5394bbacdb6ee8f6af9d7a8ce
Enrico Weigelt <enrico.weigelt@lemonize.de>
2019-11-05 11:43:00 +0100

    build file

---
+++ buildAndPush.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function buildImage() {
+  git checkout $1
+  docker build -t r1co/jenkins-docker-slave:$1
+  docker push r1co/jenkins-docker-slave:$1
+}
+
+buildImage 'node-8'
+buildImage 'node-10'
+buildImage 'node-11'
